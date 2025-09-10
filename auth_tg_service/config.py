f3aba7a3cd7f1db447fc16a605ed0cd00892be7d
Dushka <7respawnables7@gmail.com>
2025-09-10 20:22:50 +0000

    edit limit 200 sessions

--- auth_tg_service/config.py
+++ auth_tg_service/config.py
@@ -8,7 +8,7 @@ MONGO_DB = os.getenv("MONGO_DB", "tg_auth_service")
 # Используем правильный URL для Redis Docker контейнера
 REDIS_BROKER_URL = os.getenv("REDIS_BROKER_URL", "redis://:Ollama12357985@127.0.0.1:14571/1")
 SESSION_DIR = os.getenv("SESSION_DIR", "sessions/")
-MAX_CHANNELS_PER_ACCOUNT = int(os.getenv("MAX_CHANNELS_PER_ACCOUNT", 100)) 
+MAX_CHANNELS_PER_ACCOUNT = int(os.getenv("MAX_CHANNELS_PER_ACCOUNT", 200)) 
 BLACKBOX_MONGO_URI = os.getenv("BLACKBOX_MONGO_URI", "mongodb://localhost:27017/")
 BLACKBOX_DB = os.getenv("BLACKBOX_DB", "blackbox") 
 API_ID = int(os.getenv("API_ID", "123456"))
